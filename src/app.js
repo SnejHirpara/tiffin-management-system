ddb4f16db6a41aa652ebd6aed46e51619d8728b8
Snej Hirpara <snejhirpara123@gmail.com>
2025-05-22 00:17:46 +0530

    Added cookies for authentication purpose by using JWT Tokens

--- src/app.js
+++ src/app.js
@@ -1,38 +1,41 @@
-import express from 'express';
-import userRouter from './routes/user.routes.js';
-import tiffinRouter from './routes/tiffin.routes.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import userRouter from "./routes/user.routes.js";
+import tiffinRouter from "./routes/tiffin.routes.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
 
 const allowedOrigins = [
-    "https://tiffin-management-system.onrender.com",
-    /^http:\/\/localhost:\d+$/
+  "https://tiffin-management-system.onrender.com",
+  /^http:\/\/localhost:\d+$/,
 ];
 
-app.use(cors({
-    origin: function(origin, callback) {
-        if(!origin) return callback(null, true);
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
 
-        const isAllowed = allowedOrigins.some((validOrigin) => {
-            if(typeof validOrigin === 'string') return validOrigin === origin;
-            if(validOrigin instanceof RegExp) return validOrigin.test(origin);
+      const isAllowed = allowedOrigins.some((validOrigin) => {
+        if (typeof validOrigin === "string") return validOrigin === origin;
+        if (validOrigin instanceof RegExp) return validOrigin.test(origin);
 
-            return false;
-        });
+        return false;
+      });
 
-        if(isAllowed) callback(null, true);
-        else callback(new Error("Not allowed by CORS"));
-    }
-}));
+      if (isAllowed) callback(null, true);
+      else callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 
-app.use(express.json({ limit: '16kb' }))
-app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/tiffins', tiffinRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tiffins", tiffinRouter);
 
 export { app };
